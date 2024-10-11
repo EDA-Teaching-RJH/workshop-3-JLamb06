d0e73b041df982e2f07636080d0cb9443df9444a
JLamb06 <jcwl2@kent.ac.uk>
2024-10-11 09:23:58 +0000

    Workshop 3 part 2

--- partTwo.py
+++ partTwo.py
@@ -1,1 +1,15 @@
+import random
+
+guessed = False
+
+secret_number = random.randint(1,10)
+
+while guessed == False:
+    number = int(input("Guess a number between 1 and 10. "))
+    if number == secret_number:
+        print("You guessed the number!")
+        guessed = True
+    else:
+        print("You didn't guess correctly")
+
 
