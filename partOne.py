b8c3d9f5edf52ae5b70f7df17b8f87a375926789
JLamb06 <jcwl2@kent.ac.uk>
2024-10-11 09:11:33 +0000

    Workshop 3 part 1

--- partOne.py
+++ partOne.py
@@ -1,1 +1,8 @@
+age = int(input("How old are you? "))
+if age > 0 and age < 18:
+    print("You are a child")
+elif age > 0 and age >= 18:
+    print("You are an adult")
+else:
+    print ("You dont exist")
 
