d1bd2699d03b846a272f1d7a3482a611ec7dcf5a
JLamb06 <jcwl2@kent.ac.uk>
2024-10-11 10:13:00 +0000

    Workshop 3 Part 6

--- partSix.py
+++ partSix.py
@@ -1,1 +1,11 @@
+age = int(input("How old are you? "))
+student = input("Are you a student? ")
 
+if (student == "yes" or student == "Yes") and (age < 65 or age >= 12):
+    print("The ticket will be £8")
+elif age >= 65 or age < 12 and age > 0:
+    print("The ticket will be £5")
+elif (student == "no" or student == "No") and (age <= 64 or age >= 12):
+    print("The ticket will be £10")
+else:
+    print("You're not born yet")
