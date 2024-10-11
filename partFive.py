6dac059c41f96424e3df17e30b99581be6f1cbb9
JLamb06 <jcwl2@kent.ac.uk>
2024-10-11 09:56:36 +0000

    Workshop 3 part 5

--- partFive.py
+++ partFive.py
@@ -1,1 +1,8 @@
+day = input("Enter a day of the week: ")
 
+if day == "saturday" or day == "Saturday" or day == "Sunday" or day == "sunday":
+    print("It's the weekend")
+elif day == "Monday" or day == "monday" or day == "Tuesday" or day == "tuesday" or day == "wednesday" or day == "Wednesday" or day == "Thursday" or day == "thursday" or day == "Friday" or day == "friday":
+    print("It's a weekday") 
+else:
+    print("Thats not a day of the week")
