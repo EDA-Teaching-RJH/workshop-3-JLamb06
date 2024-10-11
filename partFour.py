db90de11dbd4b86061a23cae1e7c3091929fe523
JLamb06 <jcwl2@kent.ac.uk>
2024-10-11 09:45:56 +0000

    Workshop 3 part 4

--- partFour.py
+++ partFour.py
@@ -1,1 +1,10 @@
+username = ("admin")
+password = ("password123")
 
+URusername = input("Enter your password: ")
+URpassword = input("Enter your password: ")
+
+if URusername == username and URpassword == password:
+    print("Access Granted")
+else:
+    print("Access Denied")
