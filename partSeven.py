33f5fbd306b0d8e545b43b9ea0d0bb3d07bcf087
JLamb06 <jcwl2@kent.ac.uk>
2024-10-11 10:36:31 +0000

    Workshop 3 part 7

--- partSeven.py
+++ partSeven.py
@@ -1,1 +1,41 @@
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+operator1 = input("Enter the operator: ")
+tf = False
+
+while tf == False:
+    match operator1:
+        case "+":
+            result = num1 + num2
+            print(f"{num1} + {num2} = {result}")
+            tf = True
+        case "-":
+            result = num1 - num2
+            print(f"{num1} - {num2} = {result}")
+            tf = True
+        case "*":
+            result = num1 * num2
+            print(f"{num1} * {num2} = {result}")
+            tf = True
+        case "/":
+            if num1 == 0 or num2 == 0:
+                print("You may not divide by 0")
+                tf = True
+            else: 
+                result = num1 / num2
+                print(f"{num1} / {num2} = {result}")
+                tf = True
+        case "^":
+            result = num1 ^ num2
+            print(f"{num1} ^ {num2} = {result}")
+            tf = True
+        case "%":
+            result = num1 % num2
+            print(f"{num1} % {num2} = {result}")
+            tf = True
+        case "quit":
+            print("Quitting the application")
+            break
+
+        
 
